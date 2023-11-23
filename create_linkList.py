d72a682b2c31459dd0f90cbfb0cfd05bc0b80d36
nkserveren26 <norinoritoshi@outlook.jp>
2023-11-23 23:48:27 +0900

    correct create_linkList.py

--- create_linkList.py
+++ create_linkList.py
@@ -1,6 +1,13 @@
-# Markdownファイルから内容を読み込む（ファイルのパスを適切に設定）
-file_path = './README.md'
-with open(file_path, 'r', encoding='utf-8') as file:
-    markdown_content = file.read()
 
-print(markdown_content)
+
+def main():
+    # Markdownファイルから内容を読み込む（ファイルのパスを適切に設定）
+    file_path = './README.md'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        markdown_content = file.read()
+
+    print(markdown_content)
+
+
+if __name__ == "__main__":
+    main()
