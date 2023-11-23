f3228ffbf31dd13f95138f532d3c2d0bd477dc3d
nkserveren26 <norinoritoshi@outlook.jp>
2023-11-23 23:42:49 +0900

    create create_linkList.py

---
+++ create_linkList.py
@@ -0,0 +1,6 @@
+# Markdownファイルから内容を読み込む（ファイルのパスを適切に設定）
+file_path = './README.md'
+with open(file_path, 'r', encoding='utf-8') as file:
+    markdown_content = file.read()
+
+print(markdown_content)
