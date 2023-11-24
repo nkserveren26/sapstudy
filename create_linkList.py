dabe0a984b576e221094acd1ec47ab723ea64823
nkserveren26 <norinoritoshi@outlook.jp>
2023-11-24 23:57:38 +0900

    correct create_linkList

--- create_linkList.py
+++ create_linkList.py
@@ -6,11 +6,16 @@ def main():
     with open(file_path, 'r', encoding='utf-8') as file:
         markdown_content = file.read()
 
-    pattern = re.compile(r'^#{1,3}\s+(.*?)\s*\n', re.MULTILINE)
+    # 見出しを抽出する正規表現
+    pattern = re.compile(r'^(#{1,3})\s+(.*?)\s*\n', re.MULTILINE)
+
+    # マッチングした見出しを取得
     matches = pattern.finditer(markdown_content)
     
+    #取得した見出しをループしてリンクリストを生成
     for match in matches:
-        heading = match.group(1)
+        level = len(match.group(1)) #見出しの階層
+        heading = match.group(1) #見出しのタイトル
         if heading not in ["AWS Solution Architect Professional 勉強メモ", "目次"]:
             link = '- [' + heading + '](#' + heading.lower().replace(' ', '-') + ')'
             print(link)
