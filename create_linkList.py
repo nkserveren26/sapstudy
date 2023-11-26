f6e3fb9e9db71e438a5a54cfaad6b57b66f147bb
nkserveren26 <norinoritoshi@outlook.jp>
2023-11-26 16:42:57 +0900

    correct create_linkList

--- create_linkList.py
+++ create_linkList.py
@@ -1,11 +1,11 @@
 import re
 
-def generateLinkList(markdown_content: str) -> str:
+def generateLinkList(readme_content: str) -> str:
     # 見出しを抽出する正規表現
     pattern = re.compile(r'^(#{1,3})\s+(.*?)\s*\n', re.MULTILINE)
 
     # マッチングした見出しを取得
-    matches = pattern.finditer(markdown_content)
+    matches = pattern.finditer(readme_content)
     
     #取得した見出しをループしてリンクリストを生成
     link_list = []
@@ -27,12 +27,20 @@ def main():
     # Markdownファイルから内容を読み込む（ファイルのパスを適切に設定）
     file_path = './README.md'
     with open(file_path, 'r', encoding='utf-8') as file:
-        markdown_content = file.read()
+        readme_content = file.read()
 
     #リンク一覧を生成
-    link_result = generateLinkList(markdown_content)
-    print(link_result)
-            
+    link_result = generateLinkList(readme_content)
+
+    
+    # 正規表現パターンを使用して目次の開始位置と終了位置を検索
+    toc_start_match = re.search(r'^##\s*目次', readme_content, re.MULTILINE)
+    toc_end_match = re.search(r'^##\s*', readme_content[toc_start_match.end():], re.MULTILINE)
+
+    # 目次の開始位置から終了位置までの範囲を切り出す
+    toc_start_index = toc_start_match.start()
+    toc_end_index = toc_start_match.end() + toc_end_match.start()
+    
 
 if __name__ == "__main__":
     main()
