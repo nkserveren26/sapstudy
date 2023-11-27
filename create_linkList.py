0fdb1a75f68360899a31aab1b5bd53e55f27d7f0
nkserveren26 <norinoritoshi@outlook.jp>
2023-11-27 22:57:30 +0900

    correct create_linkList

--- create_linkList.py
+++ create_linkList.py
@@ -2,7 +2,7 @@ import re
 
 def generateLinkList(readme_content: str) -> str:
     # 見出しを抽出する正規表現
-    pattern = re.compile(r'^(#{1,3})\s+(.*?)\s*\n', re.MULTILINE)
+    pattern = re.compile(r'^(#{1,4})\s+(.*?)\s*\n', re.MULTILINE)
 
     # マッチングした見出しを取得
     matches = pattern.finditer(readme_content)
@@ -23,15 +23,7 @@ def generateLinkList(readme_content: str) -> str:
     return result
 
 
-def main():
-    # Markdownファイルから内容を読み込む（ファイルのパスを適切に設定）
-    file_path = './README.md'
-    with open(file_path, 'r', encoding='utf-8') as file:
-        readme_content = file.read()
-
-    #リンク一覧を生成
-    link_result = generateLinkList(readme_content)
-    
+def rewriteReadme(readme_path: str, readme_content: str, link_list: str):
     # 正規表現パターンを使用して目次の開始位置と終了位置を検索
     toc_start_match = re.search(r'^##\s*目次', readme_content, re.MULTILINE)
     toc_end_match = re.search(r'^##\s*', readme_content[toc_start_match.end():], re.MULTILINE)
@@ -44,14 +36,27 @@ def main():
     new_readme_content = (
         readme_content[:toc_start_index]
         + '## 目次\n\n'
-        + link_result
+        + link_list
         + '\n\n<br>\n\n'
         + readme_content[toc_end_index:]
     )
 
     # 書き込んだ内容でREADMEファイルを上書き
-    with open(file_path, 'w', encoding='utf-8') as readme_file:
+    with open(readme_path, 'w', encoding='utf-8') as readme_file:
         readme_file.write(new_readme_content)
+
+
+def main():
+    # Markdownファイルから内容を読み込む（ファイルのパスを適切に設定）
+    file_path = './README.md'
+    with open(file_path, 'r', encoding='utf-8') as file:
+        readme_content = file.read()
+
+    #リンク一覧を生成
+    link_result = generateLinkList(readme_content)
+
+    #READMEファイルを上書き
+    rewriteReadme(file_path, readme_content, link_result)
     
 
 if __name__ == "__main__":
